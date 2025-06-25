bcf480be8e65bfb9bb9c7ef44afb78e75f40c70d
Codehub169 <codehub@169pi.com>
2025-06-25 11:58:34 +0530

    Applied user request:
    This code was run inside of a docker container, the container stopped due to some issue or something else happenedThis was the server log:
    
    ./startup.sh: line 6: uvicorn: command not foundFix this

--- startup.sh
+++ startup.sh
@@ -2,5 +2,8 @@
 # Navigate to the backend application directory
 cd backend/app
 
+# Install Python dependencies
+pip install -r ../requirements.txt
+
 # Start the FastAPI application using uvicorn on port 9000
 uvicorn main:app --host 0.0.0.0 --port 9000
